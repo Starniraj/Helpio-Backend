733efeb73436a196d2983a5d3b0093f94e285b1d
Starniraj <singhniraj637@.com>
2026-03-24 22:55:24 +0530

    firebaseadmin fix

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -29,15 +29,27 @@
 
 //New code 401 auth solved no local cache used from line 31
 
+// const admin = require("firebase-admin");
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert({
+//       projectId: process.env.FIREBASE_PROJECT_ID,
+//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//     }),
+//   });
+// }
+
+// module.exports = admin;
+
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
